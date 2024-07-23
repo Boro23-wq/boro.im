d7dc2b36ea95fa67e9d0f1568a69a2f20dc8bb9e
Sintu Boro <sintuboro@boro.lan>
2024-07-23 16:13:52 -0500

    removed next ui table and added custom

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,10 @@
 import type { Config } from "tailwindcss";
-const { nextui } = require("@nextui-org/theme");
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/table.js",
   ],
   theme: {
     extend: {
@@ -32,7 +30,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [nextui()],
+  plugins: [],
   darkMode: "class",
 };
 
